a560037b497d762c5405f6e980881e4a33286804
hongyaohongyao <1207105685@qq.com>
2019-02-25 16:01:42 +0800

    webpack-dev-server实现的本地服务器

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import $ from 'jquery'
 import Vue from '../node_modules/vue/dist/vue.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import axios from 'axios'
+import vaxios from 'vue-axios'
+
+Vue.use(vaxios, axios)
+
 Vue.component("nitem", {
 	template: '#nitem',
 	data() {
 		return {
-			flag0:true
+			flag0: true
 		}
 	},
 	props: ['anews']
@@ -21,7 +26,13 @@ var vm = new Vue({
 
 	},
 	created() {
-		this.newslist = new Array({
+		this.axios.get('./txt/kong.txt').then(res => {
+			/* console.log(res.data) */
+			this.newslist = res.data
+		}).catch(err => {
+			console.log("error:" + err)
+		})
+		/* this.newslist = new Array({
 			id: "1",
 			title: "123",
 			update: new Date().toLocaleDateString(),
@@ -33,6 +44,6 @@ var vm = new Vue({
 			update: new Date().toLocaleDateString(),
 			author: "hongyao2",
 			content: "hahahahah"
-		})
+		}) */
 	}
 })
